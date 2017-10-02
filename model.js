26fabdf88800bde48a97cb5b082286bf99650141
briandhkim <w1057216@gmail.com>
2017-10-02 11:14:44 -0700

    initial commit

---
+++ model.js
@@ -0,0 +1,25 @@
+function Model(){
+	this.compRandPick = '';
+	// this.gameReset = false;
+
+	this.randomCompPick = function(){		//returns randomly generated computer pick
+		var randPick = Math.floor((Math.random()*3)+1);
+		switch(randPick){
+			case 1:
+				this.compRandPick = 'rock';
+				return this.compRandPick;
+			case 2:
+				this.compRandPick = 'scissor';
+				return this.compRandPick;
+			case 3:
+				this.compRandPick = 'paper';
+				return this.compRandPick;
+		} 
+	};
+
+	this.resetGame = function(){	//sets compRandPick to empty string
+		this.compRandPick = '';		//gameReset needs to be reset to false in controller
+		// this.gameReset = true;
+
+	};
+}
